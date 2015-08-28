bee567d50939352405dcdf6e1107a3c0172a571b
SSGolnev <s.s.golnev@gmail.com>
2015-08-28 07:46:10 +0300

    testik

--- modmain.lua
+++ modmain.lua
@@ -3,4 +3,3 @@ GLOBAL.TUNING.Winter_factor = GetModConfigData("Winter_factor")
 GLOBAL.TUNING.Day_factor = GetModConfigData("Day_factor")
 GLOBAL.TUNING.Dusk_factor = GetModConfigData("Dusk_factor")
 GLOBAL.TUNING.Night_factor = GetModConfigData("Night_factor")
---kjk
